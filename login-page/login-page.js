012dbe861b2dda32efd11df34099e59811f0d98f
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-16 15:54:58 -0700

    added event listener for user sign up

--- login-page/login-page.js
+++ login-page/login-page.js
@@ -1,5 +1,16 @@
+import { signupUser } from '../fetch-utils.js';
 const homeBtn = document.getElementById('home');
+const newUser = document.getElementById('sign-up');
 
 homeBtn.addEventListener('click', () => {
     location.replace('../');
+});
+
+newUser.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(newUser);
+    const user = await signupUser(data.get('email'), data.get('password'));
+    if (user) {
+        location.replace('../create-page');
+    } 
 });
