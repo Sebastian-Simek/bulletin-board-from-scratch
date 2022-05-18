71eb28640b53b5807749a0c05bafb3a60e061b6a
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-18 09:25:01 -0700

    added render function and tests

--- render-utils.js
+++ render-utils.js
@@ -2,13 +2,16 @@
 export function renderPosts(craigslist_posts) {
     
     const div = document.createElement('div');
+    
     const pTitle = document.createElement('p');
-    const pDescription = document.createElement('p');
-    const pContact = document.createElement('p');
-
     pTitle.textContent = craigslist_posts.title;
+    
+    const pDescription = document.createElement('p');
     pDescription.textContent = craigslist_posts.description;
+    
+    const pContact = document.createElement('p');
     pContact.textContent = craigslist_posts.contact;
+
 
     div.append(pTitle, pDescription, pContact);
     
--- test/example.test.js
+++ test/example.test.js
@@ -8,12 +8,13 @@ const test = QUnit.test;
 test('Testing renderPosts function', async (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div><ul><li>My test poast</li><li>This will be a test post</li><li>sebastian@sebastian.com</li></ul></div>`;
+    const expected = `<div><p>My test poast</p><p>This will be a test post</p><p>sebastian@sebastian.com</p></div>`;
     //Act 
     // Call the function you're testing and set the result to a const
-    const fetch = await fetchPosts([0]);
-    const actual = renderPosts(fetch);
+    const fetch = await fetchPosts();
+    const actual = renderPosts(fetch[0]);
     //Expect
+    console.log(fetchPosts());
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
 });
