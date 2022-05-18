0f3f543721bb86182b30289eec3c9b941e7e5469
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-05-18 12:35:40 -0700

    fixed testing

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,4 @@
 // IMPORT MODULES under test here:
-import { fetchPosts } from '../fetch-utils.js';
 
 import { renderPosts } from '../render-utils.js';
 
@@ -8,11 +7,11 @@ const test = QUnit.test;
 test('Testing renderPosts function', async (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div><p>My test poast</p><p>This will be a test post</p><p>sebastian@sebastian.com</p></div>`;
+    const expected = `<div class="render-div"><p>My test poast</p><p>This will be a test post</p><p>sebastian@sebastian.com</p></div>`;
     //Act 
     // Call the function you're testing and set the result to a const
-    const fetch = await fetchPosts();
-    const actual = renderPosts(fetch[0]);
+    
+    const actual = renderPosts({ title: 'My test poast', description: 'This will be a test post', contact: 'sebastian@sebastian.com' });
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual.outerHTML, expected);
